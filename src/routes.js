12d88a99c01190ed61b540cd2fd9a974c2a0f088
Bruno Felipe <brunofsaraujo@hotmail.com>
2019-04-10 18:16:52 -0300

    Adicionado Procfile do Heroku

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const multer = require("multer");
+const multerConfig = require("./config/multer");
+
+const routes = express.Router();
+
+const BoxController = require("./controller/BoxController");
+//const FileController = require("./controller/FileController").default;
+const FileController = require("./controller/FileController");
+
+// routes.get('/teste', (req, res) => {
+//     return res.send('Hello Rocket');
+// })
+
+//GET / POST / PUT / DELETE
+
+routes.post("/boxes", BoxController.store);
+routes.get("/boxes/:id", BoxController.show);
+
+routes.post(
+    "/boxes/:id/files",
+    multer(multerConfig).single("file"),
+    FileController.store
+);
+//routes.post("/files", multer(multerConfig).single('file'), FileController.store);
+
+module.exports = routes;
