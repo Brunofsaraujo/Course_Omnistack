de04b92ebbcf7f806b855bd9bf7687a34ce007d4
Bruno Felipe <brunofsaraujo@hotmail.com>
2019-04-11 11:33:25 -0300

    Troca para dev

--- src/models/File.js
+++ src/models/File.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const File = new mongoose.Schema({
     title: {
@@ -20,7 +20,7 @@ const File = new mongoose.Schema({
 });
 
 File.virtual("url").get(function () {
-    const url = process.env.URL || 'http://localhost:3333';
+    const url = process.env.URL || "http://localhost:3333";
 
     return `${url}/files/${encodeURIComponent(this.path)}`;
 })
